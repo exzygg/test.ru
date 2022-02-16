586e950a5c75ed10195396b04383046f7160d49c
exzy <exzygg@gmail.com>
2022-02-16 11:40:09 +0500

    dwa

---
+++ test.php
@@ -0,0 +1,12 @@
+
+<form action="" method="GET">
+	<input name="First_Name">
+	<input name="Last_Name">
+	<input type="Submit">
+</form>
+
+<?
+if (!empty($_GET)) {
+	echo $_GET['First_Name'] . ' ' . $_GET['Last_Name'];
+}
+?>
